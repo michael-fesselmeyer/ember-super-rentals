4a9b08c51ef95f6241545f4fbc599151e2d55fa6
mikefess <62866256+Drofessional@users.noreply.github.com>
2023-08-04 14:23:01 -0400

    Added maps for the rental locations using mapbox API

--- app/components/rental/image.js
+++ app/components/rental/image.js
@@ -3,7 +3,7 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class RentalImageComponent extends Component {
-    @tracked isLarge = false;
+  @tracked isLarge = false;
 
   @action toggleSize() {
     this.isLarge = !this.isLarge;
--- tests/integration/components/rental/image-test.js
+++ tests/integration/components/rental/image-test.js
@@ -20,7 +20,7 @@ module('Integration | Component | rental/image', function (hooks) {
       .hasAttribute('src', '/assets/images/teaching-tomster.png')
       .hasAttribute('alt', 'Teaching Tomster');
   });
-  
+
   test('clicking on the component toggles its size', async function (assert) {
     await render(hbs`
       <Rental::Image

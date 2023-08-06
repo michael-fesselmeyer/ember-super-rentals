586211a023e108035a2d75d0049ad7c22e759bcf
mikefess <62866256+Drofessional@users.noreply.github.com>
2023-08-05 20:14:23 -0400

    feat: add upvote and downvote actions to RentalRating component

--- app/components/rental-rating.js
+++ app/components/rental-rating.js
@@ -1,7 +1,34 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
 export default class RentalRatingComponent extends Component {
   @tracked score = 0;
   @tracked userVote = 0;
+
+  @action
+  upvote() {
+    if (this.userVote === -1) {
+      // If the user previously downvoted, remove their downvote.
+      this.score++;
+    }
+    if (this.userVote !== 1) {
+      // If the user hasn't upvoted yet, add their upvote.
+      this.score++;
+      this.userVote = 1;
+    }
+  }
+
+  @action
+  downvote() {
+    if (this.userVote === 1) {
+      // If the user previously upvoted, remove their upvote.
+      this.score--;
+    }
+    if (this.userVote !== -1) {
+      // If the user hasn't downvoted yet, add their downvote.
+      this.score--;
+      this.userVote = -1;
+    }
+  }
 }

4b1103df734086dc881502923b1917621d7640c1
mikefess <62866256+Drofessional@users.noreply.github.com>
2023-08-07 12:40:35 -0400

    Updated rental-rating-test to use proper Ember testing instead of using waitFor method

--- tests/integration/components/rental-rating-test.js
+++ tests/integration/components/rental-rating-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, click, waitFor } from '@ember/test-helpers';
+import { render, click } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | rental-rating', function (hooks) {
@@ -12,15 +12,12 @@ module('Integration | Component | rental-rating', function (hooks) {
     assert.dom('.score').hasText('0');
 
     await click('.upvote-button');
-    await waitFor('.score:contains("1")', { timeout: 2000 });
     assert.dom('.score').hasText('1');
 
     await click('.downvote-button');
-    await waitFor('.score:contains("0")', { timeout: 2000 });
     assert.dom('.score').hasText('0');
 
     await click('.downvote-button');
-    await waitFor('.score:contains("-1")', { timeout: 2000 });
     assert.dom('.score').hasText('-1');
   });
 

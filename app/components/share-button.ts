563ac530eb0e6af4a70438c134428a9e35ceff34
mikefess <62866256+Drofessional@users.noreply.github.com>
2023-08-07 10:16:12 -0400

    Converted share-button JS to Typescript and added types for ShareButtonArgs

--- app/components/share-button.ts
+++ app/components/share-button.ts
@@ -1,18 +1,25 @@
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 
-const TWEET_INTENT = 'https://twitter.com/intent/tweet';
+const TWEET_INTENT: string = 'https://twitter.com/intent/tweet';
 
-export default class ShareButtonComponent extends Component {
-  @service router;
-  get currentURL() {
+interface ShareButtonArgs {
+  text?: string;
+  hashtags?: string;
+  via?: string;
+}
+
+export default class ShareButtonComponent extends Component<ShareButtonArgs> {
+  @service router!: any; // Ideally, you'd want to use the type for the Ember router service.
+
+  get currentURL(): URL {
     return new URL(this.router.currentURL, window.location.origin);
   }
 
-  get shareURL() {
+  get shareURL(): URL {
     let url = new URL(TWEET_INTENT);
 
-    url.searchParams.set('url', this.currentURL);
+    url.searchParams.set('url', this.currentURL.toString());
 
     if (this.args.text) {
       url.searchParams.set('text', this.args.text);

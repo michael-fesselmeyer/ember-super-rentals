df28bebc75fc7e3998caee0935c52b634896d6ab
mikefess <62866256+Drofessional@users.noreply.github.com>
2023-08-06 21:19:00 -0400

    Updated rental-rating-test.js to add space after function declarations to fix the linting issues from Prettier

--- tests/integration/components/rental-rating-test.js
+++ tests/integration/components/rental-rating-test.js
@@ -3,10 +3,10 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Component | rental-rating', function(hooks) {
+module('Integration | Component | rental-rating', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it updates the score on upvoting and downvoting', async function(assert) {
+  test('it updates the score on upvoting and downvoting', async function (assert) {
     await render(hbs`<RentalRating />`);
 
     assert.dom('.score').hasText('0');
@@ -24,7 +24,7 @@ module('Integration | Component | rental-rating', function(hooks) {
     assert.dom('.score').hasText('-1');
   });
 
-  test('it prevents double-voting', async function(assert) {
+  test('it prevents double-voting', async function (assert) {
     await render(hbs`<RentalRating />`);
 
     await this.element.querySelector('.upvote-button').click();
